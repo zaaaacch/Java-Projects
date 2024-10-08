83b05eda1aa5b8a77d710fa1b865846aab8b8d46
zach beattie <zacharybeattiefl@gmail.com>
2024-10-08 15:55:02 -0400

    RoundingDoubles project complete

--- unit3/src/roundingdoubles/RoundingDoubles.java
+++ unit3/src/roundingdoubles/RoundingDoubles.java
@@ -1,7 +1,18 @@
 package roundingdoubles;
-
+import java.util.Scanner;
 public class RoundingDoubles {
 	public static void main(String[] args) {
-		
+		Scanner keyboard = new Scanner(System.in);
+		double num;
+		int newNum;
+		System.out.print("What is number? ");
+		num = keyboard.nextDouble();
+		keyboard.close();
+		if (num >= 0) {
+			newNum = (int) (num + 0.5);
+		} else {
+			newNum = (int) (num - 0.5);
+		}
+		System.out.println(newNum);
 	}
 }

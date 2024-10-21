1315c50b9f305c1b6eee5cd659e3412ca553575a
zach beattie <zacharybeattiefl@gmail.com>
2024-10-20 21:15:20 -0400

    ElseIf created and finished
    ElseIf calculates a letter grade based on a grade input by user

--- unit3/src/elseif/ElseIf.java
+++ unit3/src/elseif/ElseIf.java
@@ -9,6 +9,14 @@ public class ElseIf {
 		input.close();
 		if (grade >= 89.5) {
 			letter = "A";
-		}
-	}
+		} else if (grade >= 79.5 && grade <= 89.49999) {
+			letter = "B";
+		} else if (grade >= 69.5 && grade <= 79.49999) {
+			letter = "C";
+		} else if (grade >= 59.5 && grade <= 69.49999) {
+			letter = "D";
+		} else 
+			letter = "F";
+		System.out.println(letter);
+	} 
 }

d3b000916cd51fbce411a66a9c3c7b58df8dcedf
zach <zacharybeattiefl@gmail.com>
2024-10-22 23:17:18 -0400

    Began LittleQuiz.java
    Began work on a quiz that is based on if else statements, WIP

---
+++ Personal/src/littlequiz/LittleQuiz.java
@@ -0,0 +1,30 @@
+package littlequiz;
+import java.util.Scanner;
+public class LittleQuiz {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Are you ready for a quiz? ");
+		String isReady = input.nextLine();
+		int response;
+		int numCorrect = 0;
+		if (isReady.equals("Y")) {
+			System.out.print("Okay, here it comes!\n");
+			System.out.print("Q1) How freaky am I on a scale of 1-10?\n\t1) 10/10\n\t2) 0/10\n\t3) 1 BILLION/10\n\n> ");
+			response = input.nextInt();
+			if (response == 3) {
+				numCorrect ++;
+				System.out.println("\nThat's right! I am the freakiest of them all!");
+			} else {
+				System.out.println("\nNope! You are dumb and don't use your brain at all!");
+			}
+			System.out.print("\nQ2) Meow meow meow meow?\n\t1) meow?\n\t2) meow!\n\n> ");
+			response = input.nextInt();
+			if (response == 2) {
+				numCorrect ++;
+				System.out.println("\nYes! I love your confidence! MEOW!");
+			} else {
+				System.out.println("\nWhy do you even try at this point.");
+			}
+		}
+	}
+}

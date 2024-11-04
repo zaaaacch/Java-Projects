10250e1f8fa49f322c5c0e8b1e907af2cf8d7d71
zach <zacharybeattiefl@gmail.com>
2024-11-04 13:37:21 -0500

    change LuckySevens.java
    added a win counter to show that despite multiple wins, you should leave
    very early

--- unit4/src/luckysevens/LuckySevens.java
+++ unit4/src/luckysevens/LuckySevens.java
@@ -9,11 +9,13 @@ public class LuckySevens {
 		int rolls = 0;
 		int maxMoney = userMoney;
 		int maxCount = 0;
+		int winCount = 0;
 		while (userMoney > 0) {
 			rolls++;
 			int dice1 = (int) (Math.random() * 6 + 1);
 			int dice2 = (int) (Math.random() * 6 + 1);
 			if (dice1 + dice2 == 7) {
+				winCount++;
 				userMoney += 4;
 				if (userMoney > maxMoney) {
 					maxMoney = userMoney;
@@ -24,6 +26,7 @@ public class LuckySevens {
 			}
 		}
 		System.out.println("You are broke after " + rolls + " rolls.");
+		System.out.println("You won " + winCount + " times!");
 		System.out.print("You should have quit after " + maxCount + " rolls when you had $" + maxMoney);
 	}
 }

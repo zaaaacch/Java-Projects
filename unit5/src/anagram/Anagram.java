5a50404c7a7f99cbf0fb94dc3d9836063adb8a86
Zach Beattie <zacharybeattiefl@gmail.com>
2024-11-22 14:05:18 -0500

    Makes Anagram.java much more efficient
    Removes all instances of the current letter after checking in order to
    reduce the amount of checks

--- unit5/src/anagram/Anagram.java
+++ unit5/src/anagram/Anagram.java
@@ -2,35 +2,45 @@ package anagram;
 
 public class Anagram {
 	public static boolean validAnagram(String str1, String str2) {
-		// Returns false if the length of str1 and str2 are not equal
+		// Creates Strings that have no nonLetters
 		String noSpaceStr1 = nonLetters(str1);
 		String noSpaceStr2 = nonLetters(str2);
+		
+		// Returns false if the length of str1 and str2 are different
 		if (noSpaceStr1.length() != noSpaceStr2.length()) {
 			return false;
 		}
+		// Iterates through each letter and checks occurrences
 		for (int i = 0; i < noSpaceStr1.length(); i++) {
 			char currentChar = noSpaceStr1.charAt(i);
 			
-			int countInStr1 = countOccurences(noSpaceStr1, currentChar);
-			int countInStr2 = countOccurences(noSpaceStr2, currentChar);
-			
+			// Calls countOccurences
+			int countInStr1 = countOccurrences(noSpaceStr1, currentChar);
+			int countInStr2 = countOccurrences(noSpaceStr2, currentChar);
+			noSpaceStr1 = removeLetter(noSpaceStr1, currentChar);
+			noSpaceStr2 = removeLetter(noSpaceStr2, currentChar);
+			// Returns false if the amount of occurrence of the letters are different
 			if (countInStr1 != countInStr2) {
 				return false;
 			}
 		}
+		// Returns true if the loop runs completely through, meaning each letter occurred the same amount of times
 		return true;
 	}
 	
-	public static int countOccurences(String str, char ch) {
+	// Creates a method that increments count for the amount of cha
+	public static int countOccurrences(String str, char ch) {
 		int count = 0;
 		for (int i = 0; i < str.length(); i++) {
 			if (str.charAt(i) == ch) {
 				count++;
 			}
+			
 		}
 		return count;
 	}
 	
+	// Creates a method that returns a string without any nonletter characters
 	public static String nonLetters(String str) {
 		int len = str.length();
 		String result = "";
@@ -41,5 +51,9 @@ public class Anagram {
 			}
 		}
 		return result;
+	}
+	
+	public static String removeLetter(String str, char ch) {
+		return str.replace(String.valueOf(ch), "");
 	}
 }

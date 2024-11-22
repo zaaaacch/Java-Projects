d598f320b7ab7e731f63c1f5fa3a634f62f4d748
Zach Beattie <zacharybeattiefl@gmail.com>
2024-11-22 13:30:05 -0500

    fixed RemoveNonLetters.java

--- unit5/src/removenonletters/RemoveNonLetters.java
+++ unit5/src/removenonletters/RemoveNonLetters.java
@@ -3,11 +3,12 @@ package removenonletters;
 public class RemoveNonLetters {
 	public static String nonLetters(String str) {
 		int len = str.length();
+		String strLower = str.toLowerCase();
 		String result = "";
 		for (int i = 0; i < len; i++) {
-			String letter = str.substring(i, i+1);
+			String letter = strLower.substring(i, i+1);
 			if (letter.compareTo("a") >= 0 && letter.compareTo("z") <= 0) {
-				result += str.substring(i, i+1);
+				result += strLower.substring(i, i+1);
 			}
 		}
 		return result;
